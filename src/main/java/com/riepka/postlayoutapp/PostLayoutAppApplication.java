3230e45016e9683c8f02f4b405cb5f0b70af7fd9
repka <andrii.riepka@nixsolutions.com>
2023-09-17 15:45:39 +0300

    init setup

---
+++ src/main/java/com/riepka/postlayoutapp/PostLayoutAppApplication.java
@@ -0,0 +1,13 @@
+package com.riepka.postlayoutapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostLayoutAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostLayoutAppApplication.class, args);
+	}
+
+}
