b2c173ce935c84e54116f2d08da4f4839e32fda5
andrii.riepka <andrii.riepka@nixsolutions.com>
2023-09-24 10:42:56 +0300

    Resource changed

--- src/main/java/com/riepka/postlayoutapi/resources/PostLayoutResource.java
+++ src/main/java/com/riepka/postlayoutapi/resources/PostLayoutResource.java
@@ -8,16 +8,18 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("post-layout")
+@RestController
+@RequestMapping("/api/v1/post-layout")
 @RequiredArgsConstructor
 public class PostLayoutResource {
 
   private final PostLayoutService service;
 
   @PostMapping
-  private List<PostLayoutOption> calculateLayout(@RequestBody @Valid PostLayoutInput input) {
+  public List<PostLayoutOption> calculateLayout(@RequestBody @Valid PostLayoutInput input) {
     return service.calcPostLayout(input);
   }
 }
