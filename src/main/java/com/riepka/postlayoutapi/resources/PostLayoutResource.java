15de42376e63de9de7e1625ba186a1c45cae2c4d
andrii.riepka <andrii.riepka@nixsolutions.com>
2023-09-25 09:51:46 +0300

    Cross origin issue resolved

--- src/main/java/com/riepka/postlayoutapi/resources/PostLayoutResource.java
+++ src/main/java/com/riepka/postlayoutapi/resources/PostLayoutResource.java
@@ -6,11 +6,13 @@ import com.riepka.postlayoutapi.services.PostLayoutService;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/post-layout")
 @RequiredArgsConstructor
